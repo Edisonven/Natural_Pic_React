261ef36d6a3e41ea46f230f6e8591f6e833dedc3
Edison <aalevenegass@gmail.com>
2024-02-01 14:10:52 -0300

    Agregando comentario al código

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,22 +3,25 @@ import { GalleryContext } from "../context/GalleryContext";
 import IconHeart from "./IconHeart";
 
 const Gallery = () => {
+  //estado manejado desde un contexto global para cambiarlo como sea requerido
   const { apiData, setApiData } = useContext(GalleryContext);
 
+  //Función que se llama al hacer click en la imagen según su índice, y cambia la propiedad liked a true o false según el click
   const handleImageClick = (index) => {
+    //constante que almacena una copia del arrelo original, además crea una nueva versión del objeto con sus propiedades y se cambia la propiedad liked al inverso del valor actual (true o false)
     const newData = [...apiData];
-    newData[index] = {
-      ...newData[index],
-      liked: !newData[index].liked,
-    };
+    newData[index] = { ...newData[index], liked: !newData[index].liked };
+    //actualiza el estado en base a las funciones disparadas con el click
     setApiData(newData);
   };
 
   return (
     <div className="gallery grid-columns-5 p-3">
+      {/* Se hace un mapeo del estado global actual */}
       {apiData.map((imagen, index) => (
         <div key={imagen.id} className="img__container">
           <img
+            //funcion llamada creada más arriba que ejecuta los cambios y actualiza el estado
             onClick={() => handleImageClick(index)}
             src={imagen.src.original}
             alt=""
@@ -26,6 +29,7 @@ const Gallery = () => {
           />
 
           <h5 className="img__description">{imagen.alt}</h5>
+          {/* Componente que recibe la propiedad liked y la funcion handleImageClick para cambiar el ícono de color segun la propiedad liked (true, false)*/}
           <IconHeart
             liked={imagen.liked}
             onHeartClick={() => handleImageClick(index)}
--- src/views/Favorites.jsx
+++ src/views/Favorites.jsx
@@ -2,12 +2,16 @@ import React, { useContext } from "react";
 import { GalleryContext } from "../context/GalleryContext";
 
 const Favorites = () => {
+  //estado manejado desde un contexto global para cambiarlo como sea requerido
   const { apiData, setApiData } = useContext(GalleryContext);
 
+  //variable que almacena el estado filtrado según su propiedad liked(si es igual a true)
   const favoriteImages = apiData.filter((imagen) => imagen.liked);
 
+  //Función que se llama al hacer click en la imagen según id, y cambia la propiedad liked a false para quitarla de la sección favoritos
   const handleImageClick = (id) => {
     const newData = apiData.map((imagen) => {
+      //Condicional que evalúa la propiedad id de la imagen dinámicamente según el click, y le cambia su propiedad liked a false
       if (imagen.id === id) {
         return {
           ...imagen,
@@ -16,7 +20,7 @@ const Favorites = () => {
       }
       return imagen;
     });
-
+    //Se setea el estado con los nuevos datos mapeados
     setApiData(newData);
   };
 
@@ -24,6 +28,7 @@ const Favorites = () => {
     <div>
       <h1 className="favorites__title">Fotos favoritas</h1>
       <div className="p-3 gallery grid-columns-4">
+        {/*Mapeo del arreglo filtrado */}
         {favoriteImages.map((imagen) => (
           <div key={imagen.id} className="img__container">
             <img
