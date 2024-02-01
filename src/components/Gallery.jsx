7ce2142dba0c9cece8f96f57e21a9f5c509b8c86
Edison <aalevenegass@gmail.com>
2024-02-01 11:08:17 -0300

    Mapeando galería de imagenes en home

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,16 @@
+import { useContext } from "react";
+import { GalleryContext } from "../context/GalleryContext";
 const Gallery = () => {
-  return <div className="gallery grid-columns-5 p-3"></div>;
+  const { appiData } = useContext(GalleryContext);
+  return (
+    <div className="gallery grid-columns-5 p-3">
+      {appiData.map((imagen) => (
+        <div key={imagen.id} className="img__container">
+          <img src={imagen.src.original} alt="" className="img" />
+          <h5 className="img__description">{imagen.alt}</h5>
+        </div>
+      ))}
+    </div>
+  );
 };
 export default Gallery;
