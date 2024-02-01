c76211cdcfc40d1ed97d158e191fd78b3baa1949
Edison <aalevenegass@gmail.com>
2024-02-01 11:14:27 -0300

    Importando ícono de like

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,13 +1,16 @@
 import { useContext } from "react";
 import { GalleryContext } from "../context/GalleryContext";
+import IconHeart from "./IconHeart";
 const Gallery = () => {
   const { appiData } = useContext(GalleryContext);
+
   return (
     <div className="gallery grid-columns-5 p-3">
       {appiData.map((imagen) => (
         <div key={imagen.id} className="img__container">
           <img src={imagen.src.original} alt="" className="img" />
           <h5 className="img__description">{imagen.alt}</h5>
+          <IconHeart></IconHeart>
         </div>
       ))}
     </div>
