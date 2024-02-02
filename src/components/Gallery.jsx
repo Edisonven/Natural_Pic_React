9f39f389ece447e8236f41b77f761388c7646cdc
Edison <aalevenegass@gmail.com>
2024-02-02 17:55:05 -0300

    Cambiando parametro de búsqueda de imágenes por click de index a id

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -7,11 +7,13 @@ const Gallery = () => {
   const { apiData, setApiData } = useContext(GalleryContext);
 
   //Función que se llama al hacer click en la imagen según su índice, y cambia la propiedad liked a true o false según el click
-  const handleImageClick = (i) => {
-    //constante que almacena una copia del arrelo original, además crea una nueva versión del objeto con sus propiedades y se cambia la propiedad liked al inverso del valor actual (true o false)
+  const handleImageClick = (id) => {
     const newData = [...apiData];
-    newData[i] = { ...newData[i], liked: !newData[i].liked };
-    //actualiza el estado en base a las funciones disparadas con el click
+
+    const imagenClikeada = newData.find((imagen) => imagen.id === id);
+    if (imagenClikeada) {
+      imagenClikeada.liked = !imagenClikeada.liked;
+    }
     setApiData(newData);
   };
 
@@ -22,7 +24,7 @@ const Gallery = () => {
         <div key={imagen.id} className="img__container">
           <img
             //funcion llamada creada más arriba que ejecuta los cambios y actualiza el estado
-            onClick={() => handleImageClick(i)}
+            onClick={() => handleImageClick(imagen.id)}
             src={imagen.src.original}
             alt=""
             className="img"
@@ -32,7 +34,7 @@ const Gallery = () => {
           {/* Componente que recibe la propiedad liked y la funcion handleImageClick para cambiar el ícono de color segun la propiedad liked (true, false)*/}
           <IconHeart
             liked={imagen.liked}
-            handleImageClick={() => handleImageClick(i)}
+            handleImageClick={() => handleImageClick()}
           />
         </div>
       ))}
